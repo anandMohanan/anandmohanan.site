85ee3f1ef1f3a543833d3b5fbcfe329db882faf4
anandMohanan <ananthkvmohanan@gmail.com>
2025-01-19 23:12:01 +0530

    padding fix

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     return (
         <div className="text-center">
             <h1 className="text-4xl font-bold mb-4">Anand Mohanan</h1>
-            <p className="mb-8 lg:px-64 md:px-40 px-24">
+            <p className="mb-8 lg:px-64 md:px-40 px-8">
                 Tinkering with Linux, automating tasks, and refining my workflow are my
                 passions. Lately, I'm exploring AI agents, crafting web apps, and
                 experimenting with OCaml and Zig for fresh programming perspectives.
